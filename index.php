7365f427156bd38a6abaadfb2e815a5d1f197fe7
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-15 21:37:56 +0200

    Add routing logic to index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+define('ROOT_PATH', __DIR__);
+session_start();
+require 'includes/db.php';?>
+
+<?php
+$page = $_GET['page'] ?? 'home'; // Default to 'home' if no page is specified
+
+switch ($page) {
+    case 'home':
+        require 'public/index.php';
+        break;
+    case 'login':
+        require 'public/login.php';
+        break;
+    case 'add_post':
+        require 'admin/add_post.php';
+        break;
+    case 'post':
+        require ROOT_PATH . '/public/post.php';
+    break;
+    // Add more cases as needed...
+    default:
+        // You could show a 404 page here...
+        echo "Page not found";
+        break;
+}
+?>
