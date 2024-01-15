a831008e46a21ebb7a2a130c7d4e42636bd0714f
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-15 20:42:53 +0200

    Refactor header.php to include conditional navigation links based on admin login status

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,15 @@
+<?php session_start(); ?>
+
 <nav>
     <a href="../public/index.php">Home</a>
     |
-    <a href="../public/login.php">Admin Login</a>
-    |
-    <a href="../admin/add_post.php">Create Post</a>
-    |
-    <a href="../admin/logout.php">Logout</a>
+    <?php if (!isset($_SESSION['admin_logged_in'])): ?>
+        <a href="../public/login.php">Admin Login</a>
+    <?php else: ?>
+        <?php if ($_SERVER['SCRIPT_NAME'] != '/datalabs-blog-exercise/admin/add_post.php'): ?>
+            <a href="../admin/add_post.php">Create Post</a>
+            |
+        <?php endif; ?>
+        <a href="../admin/logout.php">Logout</a>
+    <?php endif; ?>
 </nav>
